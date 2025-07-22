7260e98e67df8f39b9dccde4d589ef12ab2013b2
Taylor Anderson <taylor01anderson@icloud.com>
2025-07-22 12:54:10 -0700

    implemented JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -56,9 +58,21 @@ router.post(
 
       await user.save();
 
-      res.send("User registered successfully");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.send("User registered successfully");
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
