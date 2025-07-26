e909e106d043d62dc5c59352330525240025427b
Taylor Anderson <taylor01anderson@icloud.com>
2025-07-26 12:17:58 -0700

    implemented a route to add a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,42 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route   GET api/posts
-// @desc    Test users route
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => {
-  res.send('Posts API Running');
-});
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res;
+    }
+  }
+);
 
 module.exports = router;
