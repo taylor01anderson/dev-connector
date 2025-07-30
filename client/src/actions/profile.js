d72df1a6122b23b661e2a9f58b94b90590456069
taylor01andgit config --global user.email taylor01anderson@icloud.com <taylor01anderson@icloud.com>
2025-07-29 19:25:36 -0700

    implemented delete profile feature

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import { useNavigate } from 'react-router-dom';
 
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  ACCOUNT_DELETED,
+  CLEAR_PROFILE,
+} from './types';
 
 // Get current user's profile
 export const getCurrentProfile = () => async (dispatch) => {
@@ -138,5 +144,74 @@ export const addExperience = (formData, navigate) => async (dispatch) => {
         status: error.response ? error.response.status : 500,
       },
     });
+  }
+};
+
+// Delete experience
+
+export const deleteExperience = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Experience Removed', 'success'));
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response ? error.response.statusText : 'Server Error',
+        status: error.response ? error.response.status : 500,
+      },
+    });
+  }
+};
+
+// Delete experience
+
+export const deleteEducation = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/profile/education/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Education Removed', 'success'));
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response ? error.response.statusText : 'Server Error',
+        status: error.response ? error.response.status : 500,
+      },
+    });
+  }
+};
+
+// Delete account & profile
+
+export const deleteAccount = (id) => async (dispatch) => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    try {
+      const res = await axios.delete(`/api/profile`);
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+
+      dispatch(setAlert('Your account has been permanently deleted'));
+    } catch (error) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: error.response ? error.response.statusText : 'Server Error',
+          status: error.response ? error.response.status : 500,
+        },
+      });
+    }
   }
 };
--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -102,7 +102,7 @@ const AddEducation = ({ addEducation }) => {
                   toggleToDateDisabled(!toDateDisabled);
                 }}
               />{' '}
-              Current Job
+              Current School
             </p>
           </div>
           <div class="form-group">
