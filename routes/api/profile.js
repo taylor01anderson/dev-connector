dae2eb25b8433225de2404bb71e28741322cd64e
taylor01andgit config --global user.email taylor01anderson@icloud.com <taylor01anderson@icloud.com>
2025-08-04 00:46:19 -0700

    fixed typo on experience validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -268,7 +268,7 @@ router.put(
     auth,
     [
       check('title', 'Title is required').not().isEmpty(),
-      check('company', 'From date is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
       check('from', 'From date is required').not().isEmpty(),
     ],
   ],
