25672728e371caafd318c603a79cf879ac11e67c
taylor01andgit config --global user.email taylor01anderson@icloud.com <taylor01anderson@icloud.com>
2025-08-03 23:33:25 -0700

    updated profile route and file name

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
-const Profile = require('../../models/Profile');
+const Profile = require('../../models/profile');
 const User = require('../../models/User');
 const Post = require('../../models/Post');
 
